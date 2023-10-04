7b9e23d4bcb5b4ee930d2f4c47ee9c8cdd503965
Gregor Purdy <gregor@abcelo.com>
2023-10-03 20:41:34 -0700

    Remove unneeded to_string() calls on CUSIP (it already implements Display)

--- examples/parse.rs
+++ examples/parse.rs
@@ -4,7 +4,7 @@ fn main() {
     let cusip_string = "023135106"; // Amazon.com Inc - Common Stock
     match cusip::parse(cusip_string) {
         Ok(cusip) => {
-            println!("Parsed CUSIP: {}", cusip.to_string()); // "023135106"
+            println!("Parsed CUSIP: {}", cusip); // "023135106"
             println!("  Issuer number: {}", cusip.issuer_num()); // "023135"
             println!("  Issue number: {}", cusip.issue_num()); // "10"
             println!("  Check digit: {}", cusip.check_digit()); // '6'
